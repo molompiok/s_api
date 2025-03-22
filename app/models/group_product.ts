e8adc327dce70e31b2ffdc37beaa93072faec077
simeon619 <sijean619@gmail.com>
2025-03-22 23:44:38 +0100

    Add GroupProduct model with relationships and additional fields for product management

---
+++ app/models/group_product.ts
@@ -0,0 +1,41 @@
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations';
+
+import Product from './product.js'
+export default class GroupProduct extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: string
+
+  @column()
+  declare product_id:string
+  
+  @column()
+  declare currency: string 
+
+  @column()
+  declare additional_price: number
+
+  @column()
+  declare stock: number
+
+  @column({ 
+    serializeAs: 'bind',
+    prepare: (value: Record<string, any> | null) => (value ? JSON.stringify(value) : null),
+  })
+  declare bind: Record<string, any> | null // {"ram": "16","taille": "XXL","couleur": "yellow"}
+
+  @belongsTo(() => Product, {
+    foreignKey: 'product_id', // Colonne dans GroupProduct
+    localKey: 'id', // Colonne dans Product
+  })
+  declare product: BelongsTo<typeof Product>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+
+}
